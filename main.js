fe6e829147fb068da233ae2efa2a730f90b449a3
Vauquelin Aurelie <aurelie.vauquelin@le-campus-numerique.fr>
2024-04-10 12:06:12 +0200

    itération 5

---
+++ main.js
@@ -0,0 +1,4 @@
+//importer le server.js
+const createServer = require("./server");
+
+createServer();
---
+++ server.js
@@ -0,0 +1,26 @@
+//demarer le server
+function start () {
+    const express = require ("express")
+    const port = process.env.PORT || 3000
+    
+    const app = express();
+    
+    app.get("/", (req, res) => { //req = requête et res = réponse
+        res.send("Got it !")
+    })
+    
+    app.listen(port, () => {
+        console.log("serveur est en ligne !");
+    })
+
+    app.use( function (req, res, next) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader('Access-Control-Allow-Methods', '*');
+        res.setHeader("Access-Control-Allow-Headers", "*");
+        next();
+         });
+
+
+}
+
+module.exports = start; // pour exporter la fonction start
